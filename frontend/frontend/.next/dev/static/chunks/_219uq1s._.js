70a99b0a82218023015f8297d9176a0de720a6d1
Aakriti Kushwaha <aakriti.kushwaha.ak@gmail.com>
2026-09-04 17:37:55 +0530

    Fix build errors - resolve merge conflicts in page.tsx

---
+++ frontend/frontend/.next/dev/static/chunks/_219uq1s._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/node_modules_1ozl1nx._.js","static/chunks/src_0zqugno._.js","static/chunks/src_app_globals_162hn9o.css","static/chunks/src_1ylu6fc._.js","static/chunks/node_modules_10is3ca._.js"],
+    source: "entry"
+});
